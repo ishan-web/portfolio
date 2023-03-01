3b84052d2e0a95f46a2b4e37860e3fdc6bc2bafb
Your Name <ishanpoudel58@gmail.com>
2023-03-01 22:16:16 +0545

    css

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
 import Header from './components/header/Header'
+import About from './components/about/About'
+import Footer from './components/footer/Footer'
+import Contact from './components/contact/Contact'
+import Experience from './components/experience/Experience'
+import Nav from './components/nav/Nav'
+import Testimonials from './components/testimonials/Testimonials'
+import Portfolio from './components/portfolio/Portfolio'
 
 const App = () => {
   return (
     <>
       <Header />
+      <About />
+      <Footer />
+      <Contact />
+      <Experience />
+      <Nav />
+      <Testimonials />
+      <Portfolio />
     </>
   )
 }
