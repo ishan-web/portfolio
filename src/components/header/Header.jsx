7d9fbc00b30cdcdc0763aa564577d26b957e12c2
Your Name <ishanpoudel58@gmail.com>
2023-03-02 22:23:05 +0545

    header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,11 +1,24 @@
 import React from 'react'
 import './header.css'
+import CTA from './CTA'
+import ME from '../../assets/me.jpg'
+import HeaderSocials from './HeaderSocials'
 
 const Header = () => {
   return (
-    <div>
-      header
-    </div>
+    <header>
+      <div className="container header_container">
+        <h5>Hello I'm</h5>
+        <h1>Ishan Paudel</h1>
+        <h5 className="text-light">Fullstack Developer</h5>
+        <CTA/>
+        <HeaderSocials />
+        <div className="me">
+          <img src={ME} alt="me"/>
+        </div>
+        <a href="#contact" className="scrool_down">Scrool Down</a>
+      </div>
+    </header>
   )
 }
 
