ca4515687a279497c2a4b2461435fa7e4cfa0ddd
Your Name <ishanpoudel58@gmail.com>
2023-03-03 22:25:11 +0545

    header complete

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -7,7 +7,7 @@ import HeaderSocials from './HeaderSocials'
 const Header = () => {
   return (
     <header>
-      <div className="container header_container">
+      <div className="container header__container">
         <h5>Hello I'm</h5>
         <h1>Ishan Paudel</h1>
         <h5 className="text-light">Fullstack Developer</h5>
@@ -16,7 +16,7 @@ const Header = () => {
         <div className="me">
           <img src={ME} alt="me"/>
         </div>
-        <a href="#contact" className="scrool_down">Scrool Down</a>
+        <a href="#contact" className="scroll__down">Scroll Down</a>
       </div>
     </header>
   )
