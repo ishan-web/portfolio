cd129de6ddc0cd847d7a758b50eb464d9d4388db
Your Name <ishanpoudel58@gmail.com>
2023-03-04 22:27:40 +0545

    navbar

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import './nav.css'
+import {AiTwotoneHome} from 'react-icons/ai'
+import {FaUserSecret} from 'react-icons/fa'
+import {ImBook} from 'react-icons/im'
+import {MdElectricalServices} from 'react-icons/md'
+import {AiFillContacts} from 'react-icons/ai'
 
-const Nav = () => {
+
+const Nav = () => { 
   return (
     <div>
-      nav
+      <a href="#" ><AiTwotoneHome/></a>
+      <a href="#" ><FaUserSecret/></a>
+      <a href="#" ><ImBook/></a>
+      <a href="#" ><MdElectricalServices/></a>
+      <a href="#" ><AiFillContacts/></a>
     </div>
   )
 }
